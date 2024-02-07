6a3cd7f1afb1d170d1c48c30fe19994761fff8c8
Benjy <benjy.ramirez@wera.com.mx>
2024-02-07 09:41:30 -0700

    prueba3

--- main.py
+++ main.py
@@ -23,3 +23,25 @@ app.add_middleware(
 def read_root():
     return {"Hello": "World"}
 
+@app.post("/cargarDocumento/{bl}/{idOrden}")
+async def cargarDocumento(bl,idOrden,files: List[UploadFile] = File(...)):
+    listdocs = []
+    try:
+        for file in files:
+            print(file.filename)
+            with open(file.filename, 'wb') as myfile:
+                content = await file.read()
+                myfile.write(content)
+                myfile.close()
+
+                if not os.path.exists('documentos/'+str(idOrden)+''):
+                    os.mkdir('documentos/'+str(idOrden)+'')
+
+                extension = os.path.splitext(str(file.filename))
+                rutaDoc = r"documentos/"+str(idOrden)+"/BL_"+str(bl)+extension[1]
+                shutil.move(file.filename,rutaDoc)
+                print('Documentos movidos')
+                return {'Res':'enviado'}
+    except Exception as e:
+        print(e)
+        return {'Res':e}
